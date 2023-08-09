1a2c87b30528b4146f95cae334ba2b045b35f9db
Tadi Prem Kumar Naidu <prempk.7172@gmail.com>
2023-08-09 22:20:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-//schema
+
 const dataSchema = {
     name: String,
     last:String,
